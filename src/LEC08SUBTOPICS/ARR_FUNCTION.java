58c61f399aa0e52acc2d317ab535cb2316b01887
Yogesh <longquest.oyscommunity@gmail.com>
2024-06-17 11:55:57 +0530

    first commit

---
+++ src/LEC08SUBTOPICS/ARR_FUNCTION.java
@@ -0,0 +1,32 @@
+package LEC08SUBTOPICS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ARR_FUNCTION {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        System.out.println(Arrays.toString(arr));
+        array(arr);
+        System.out.println("next modify");
+        modify(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void array (int[] num){
+        Scanner in = new Scanner(System.in);
+        for (int i = 0 ; i < num.length ; i++){
+            num[i]= in.nextInt();
+            System.out.print(num[i] + " ");
+        }
+    }
+    static void modify (int[] num){
+        Scanner modify = new Scanner(System.in);
+        System.out.println("enter the place out of "+ (num.length-1) +" to be modified");
+        int k = modify.nextInt();
+        System.out.println("enter the number to be replaced");
+        int a = modify.nextInt();
+        num[k] = a;
+        System.out.println(Arrays.toString(num));
+    }
+}
