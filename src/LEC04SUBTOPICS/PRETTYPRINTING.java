a76702adea3d5b0ee01b925e7ed2fd83e8286a75
Yogesh <longquest.oyscommunity@gmail.com>
2024-06-20 22:50:55 +0530

    01 commit

---
+++ src/LEC04SUBTOPICS/PRETTYPRINTING.java
@@ -0,0 +1,9 @@
+package LEC04SUBTOPICS;
+
+public class PRETTYPRINTING {
+    public static void main(String[] args) {
+        float a = 3;
+        System.out.printf("%.2f %n",a);
+        System.out.printf("Hello my name os %s and im %s", "yogesh", "cool");
+    }
+}
