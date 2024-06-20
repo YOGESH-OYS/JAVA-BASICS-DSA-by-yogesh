b02d0d7aec358d3ba8a0638166e3e42aa729bc12
Yogesh <longquest.oyscommunity@gmail.com>
2024-06-20 23:01:42 +0530

    00 commit

--- src/LEC04SUBTOPICS/PRETTYPRINTING.java
+++ src/LEC04SUBTOPICS/PRETTYPRINTING.java
@@ -4,6 +4,9 @@ public class PRETTYPRINTING {
     public static void main(String[] args) {
         float a = 3;
         System.out.printf("%.2f %n",a);
-        System.out.printf("Hello my name os %s and im %s", "yogesh", "cool");
+        System.out.printf("Hello my name os %s and im %s %n", "yogesh", "cool");
+        int a1 = 1;
+        System.out.println("a" + a1);//this prints a1 because the println actually converts the object inside the bracket into a string by string wrapper class for more look after the class of println
+
     }
 }
