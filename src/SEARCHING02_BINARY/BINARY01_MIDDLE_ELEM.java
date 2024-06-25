32cf4f0372a5460b62df34ae6af8b574bede9c5f
Yogesh <longquest.oyscommunity@gmail.com>
2024-06-25 20:41:27 +0530

    01 commit

--- src/SEARCHING02_BINARY/BINARY01_MIDDLE_ELEM.java
+++ src/SEARCHING02_BINARY/BINARY01_MIDDLE_ELEM.java
@@ -6,7 +6,7 @@ public class BINARY01_MIDDLE_ELEM {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 2, 7, 9, 10, 8, 4};
         Arrays.sort(arr);
-        int target = 4;
+        int target = 11;
         System.out.println(greater(arr, target));
     }
 
