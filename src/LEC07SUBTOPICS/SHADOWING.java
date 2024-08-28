905844b1fdd62953c2ae05c895ebb18b71f3df5e
Yogesh <longquest.oyscommunity@gmail.com>
2024-08-28 11:54:46 +0530

    Lec07 commit

--- src/LEC07SUBTOPICS/SHADOWING.java
+++ src/LEC07SUBTOPICS/SHADOWING.java
@@ -7,8 +7,12 @@ public class SHADOWING {
         int a = 20;//making the 10 as a shadow and printing the new value
         System.out.println(a);//ghosting a variable (10)
         shadow();
+        shadow(a);
     }
     static void shadow(){
         System.out.println(a);//the original global value
+    }
+    static void shadow(int s){
+        System.out.println(s+1);//the original global value
     }
 }
